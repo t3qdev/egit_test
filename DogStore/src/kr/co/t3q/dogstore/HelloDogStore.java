dbee50c9443f1f848470821c81fd2403a5af0252
been <been@DABEEN>
2016-05-16 17:30:57 +0900

    init commit
    Change-Id: I47eae348bb3cd5a7d617b986b38467685ec38e34
    Signed-off-by: been <been@DABEEN>

---
+++ DogStore/src/kr/co/t3q/dogstore/HelloDogStore.java
@@ -0,0 +1,9 @@
+package kr.co.t3q.dogstore;
+
+public class HelloDogStore {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO DOGSTORE!!");
+	}
+
+}
---
+++ PetStore/src/kr/co/t3q/petstore/HelloDogStore.java
@@ -0,0 +1,9 @@
+package kr.co.t3q.petstore;
+
+public class HelloDogStore {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO PETSTORE!!");
+	}
+
+}
